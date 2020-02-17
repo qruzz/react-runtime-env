29687d3b8bd6d4479329b693e1f8fad94add744b
qruzz <m9ssen@gmail.com>
2020-02-17 22:55:48 +0000

    feat: updated architecture

---
+++ src/test.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useCompare } from './useCompare';
+
+let RuntimeEnvContext: any = undefined;
+
+export function RuntimeEnvProvider<T>({
+	defaultEnv,
+	configPath,
+	onError,
+}: {
+	defaultEnv: T;
+	configPath: string;
+	onError: (error: Error) => void;
+}) {
+	const [env, setEnv] = React.useState<T>(defaultEnv);
+
+	const didEnvUpdate = useCompare(env);
+
+	React.useEffect(() => {
+		if (process.env.NODE_ENV !== 'development') {
+			configureRuntimeEnv<T>(configPath)
+				?.then(data =>
+					setEnv({
+						...data,
+						...process.env,
+					})
+				)
+				.catch(error => {
+					if (onError) {
+						onError(error);
+					} else {
+						throw new Error(error);
+					}
+				});
+		} else {
+			setEnv({
+				...env,
+				...process.env,
+			});
+		}
+	}, [didEnvUpdate]);
+}
+
+export function userRuntimeEnv<T>() {
+	const context = React.useContext<T>(RuntimeEnvContext as React.Context<T>);
+
+	if (context === undefined) {
+		throw new Error('useRuntimeEnv must be used within a RuntimeEnvProvider');
+	}
+
+	return context;
+}
+
+function configureRuntimeEnv<T>(configPath: string): Promise<T> | null {
+	if (fetch !== undefined) {
+		return fetch(configPath)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+				return response.json() as Promise<T>;
+			})
+			.catch(error => {
+				throw new Error(error);
+			});
+	}
+
+	return null;
+}
